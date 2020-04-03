c0290112de8215dfac1983c7ef65a697d6b0e91c
Antoine <antoine.servolin@gmail.com>
2020-04-03 15:06:01 +0200

    Antoine tests

---
+++ premiers_tests/test_comprehension_server.cpp
@@ -0,0 +1,16 @@
+#include "structure_messages.h"
+#include <cstdint>
+
+
+uint8_t* comprehension(uint8_t* input){
+	return input;
+}
+
+int main(int argc, char* argv[]) {
+	uint8_t u[] = "5a";
+	cout << comprehension(u) << endl;
+	return 0;
+}
+
+
+
