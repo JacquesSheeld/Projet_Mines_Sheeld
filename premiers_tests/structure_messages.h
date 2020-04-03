bd2f628e58d77f80cc903364869257522da414a7
Antoine <antoine.servolin@gmail.com>
2020-04-03 17:15:56 +0200

    ajout des classes de messages

--- premiers_tests/structure_messages.h
+++ premiers_tests/structure_messages.h
@@ -6,19 +6,91 @@ using namespace std;
 
 class message {
 	protected:
-		uint8_t letter;
+		uint8_t type;
 		uint64_t time;
+
+	public:
+		message();
 };
 
-class Add_message : public message {
+class Add_message : public message { // Type : A
 	protected:
-		uint16_t symbol_ID; // encodage du produit 
-		uint64_t ID;
+		uint16_t sid; // encodage du produit 
+		uint64_t qid;
 		uint32_t price;
 		uint32_t volume;
 		char status;
 	public:
-		Add_message()
+		Add_message();
 };
 
+class Control_message : public message { // Type : C
+	protected:
+		uint16_t sid; // encodage du produit 
+		char Operating_status;
+	public:
+		Control_Message();
+}
 
+class Reduce_message { // Type : D
+	public:
+		uint16_t sid; // encodage du produit 
+		uint64_t qid;
+		uint32_t volume;	
+	protected:
+		Reduce_message();
+	
+};
+
+class Execution {	// Type : E
+	public:
+		uint16_t sid; // encodage du produit 
+		uint64_t qid;
+		uint32_t volume;
+		uint64_t mid;
+	protected:
+		Execution();
+	
+};
+
+class Master_message { // Type : L
+	public:
+		uint16_t sid; // encodage du produit 
+		char symbol[8];
+		char currency[8];
+		uint8_t lot;
+		uint8_t tick;
+		char classification	
+	protected:
+		Master_message();
+	
+};
+
+class Modify_message { // Type : M
+	public:
+		uint16_t sid; // encodage du produit 
+		uint64_t qid;
+		uint64_t nid;
+		uint32_t price;
+		uint32_t volume;	
+	protected:
+		Modify_message();
+	
+};
+
+class Remote_message { // Type : R
+	public:
+		uint16_t sid; // encodage du produit 
+		uint64_t qid;
+	protected:
+		Remote_message();
+	
+};
+
+class Protocol_message { // Type : Z
+	public:
+		uint32_t version;
+	protected:
+		Protocol_message();
+	
+};
