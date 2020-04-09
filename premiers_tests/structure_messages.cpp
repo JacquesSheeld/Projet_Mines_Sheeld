297f454a50fdf8d581793e7daaaba374658ac08f
Jeremy Costanzo <jeremy.costanzo@mines-paristech.fr>
2020-04-09 16:42:41 +0200

    J'ai enlevé les indentations et ajouté la méthode display pour MasterMessage

--- premiers_tests/structure_messages.cpp
+++ premiers_tests/structure_messages.cpp
@@ -8,64 +8,67 @@ message::message(uint8_t _type, uint64_t _time): type(_type), time(_time) {};
 // ADD
 
 Add_message::Add_message(uint8_t _type, uint64_t _time, uint16_t _sid, uint64_t _qid, uint32_t _price, uint32_t _volume, char _status):
-																															message(_type, _time),
-																															sid(_sid),
-																															qid(_qid),
-																															price(_price),
-																															volume(_volume),
-																															status(_status) {};
-																													
+	message(_type, _time),
+	sid(_sid),
+	qid(_qid),
+	price(_price),
+	volume(_volume),
+	status(_status) {};
+								
 void Add_message::Display(){
-	cout << "type :" << type << endl << "time :" << time << endl << "sid :" << sid << endl << "qid :" << qid << endl <<  "price :" << price << endl << "volume :" << volume << endl << "status :" << status << endl << endl;
+	cout << "type : " << type << endl << "time : " << time << endl << "sid : " << sid << endl << "qid : " << qid << endl <<  "price : " << price << endl << "volume : " << volume << endl << "status : " << status << endl << endl;
 }
 
 // CONTROL
 
 Control_message::Control_message(uint8_t _type, uint64_t _time, uint16_t _sid, char _status):
-																					message(_type, _time),
-																					sid(_sid),
-																					status(_status) {};
+	message(_type, _time),
+	sid(_sid),
+	status(_status) {};
 
 void Control_message::Display(){
-	cout << "type :" << type << endl << "time :" << time << endl << "sid :" << sid << endl << "status :" << status << endl << endl;
+	cout << "type : " << type << endl << "time : " << time << endl << "sid : " << sid << endl << "status : " << status << endl << endl;
 }
 
 // REDUCE
 
 Reduce_message::Reduce_message(uint8_t _type, uint64_t _time, uint16_t _sid, uint64_t _qid, uint32_t _volume):
-																					message(_type, _time),
-																					sid(_sid),
-																					qid(_qid),
-																					volume(_volume) {};
+	message(_type, _time),
+	sid(_sid),
+	qid(_qid),
+	volume(_volume) {};
 
 void Reduce_message::Display(){
-	cout << "type :" << type << endl << "time :" << time << endl << "sid :" << sid << endl << "qid :" << qid << endl << "volume :" << volume << endl << endl;
+	cout << "type : " << type << endl << "time : " << time << endl << "sid : " << sid << endl << "qid : " << qid << endl << "volume : " << volume << endl << endl;
 }
 
 // EXECUTION
 
 Execution_message::Execution_message(uint8_t _type, uint64_t _time, uint16_t _sid, uint64_t _qid, uint32_t _volume, uint64_t _mid):
-																					message(_type, _time),
-																					sid(_sid),
-																					qid(_qid),
-																					volume(_volume),
-																					mid(_mid) {};
+	message(_type, _time),
+	sid(_sid),
+	qid(_qid),
+	volume(_volume),
+	mid(_mid) {};
 
 void Execution_message::Display(){
-	cout << "type :" << type << endl << "time :" << time << endl << "sid :" << sid << endl << "qid :" << qid << endl << "volume :" << volume << endl << "mid :" << mid << endl << endl;
+	cout << "type : " << type << endl << "time : " << time << endl << "sid : " << sid << endl << "qid : " << qid << endl << "volume : " << volume << endl << "mid : " << mid << endl << endl;
 }
 
 // MASTER
 
 Master_message::Master_message(uint8_t _type, uint64_t _time, uint16_t _sid, char* _symbol, char* _currency, uint8_t _lot, uint8_t _tick, char _classification):
-																					message(_type, _time),
-																					sid(_sid),
-																					symbol(_symbol),
-																					currency(_currency),
-																					lot(_lot),
-																					tick(_tick),
-																					classification(_classification) {};
+	message(_type, _time),
+	sid(_sid),
+	symbol(_symbol),
+	currency(_currency),
+	lot(_lot),
+	tick(_tick),
+	classification(_classification) {};
 
+void Master_message::Display(){
+	cout << "type : " << type << endl << "time : " << time << endl << "sid : " << sid << endl << "symbol : " << symbol << endl << "currency : " << currency << endl << "lot : " << lot << endl << "tick : " << tick << endl << "classification : " << classification << endl << endl;
+}
 // MODIFY
 
 Modify_message::Modify_message(uint8_t _type, uint64_t _time, uint16_t _sid, uint64_t _qid, uint64_t _nid, uint32_t _price, uint32_t _volume):																				
@@ -77,7 +80,7 @@ Modify_message::Modify_message(uint8_t _type, uint64_t _time, uint16_t _sid, uin
 	volume(_volume) {};
 
 void Modify_message::Display(){
-	cout << "type :" << type << endl << "time :" << time << endl << "sid :" << sid << endl << "qid :" << qid << endl << "nid :" << nid << endl << "price :" << price << endl << "volume :" << volume << endl << endl;
+	cout << "type : " << type << endl << "time : " << time << endl << "sid : " << sid << endl << "qid : " << qid << endl << "nid : " << nid << endl << "price : " << price << endl << "volume : " << volume << endl << endl;
 }
 
 // REMOTE
@@ -88,7 +91,7 @@ Remote_message::Remote_message(uint8_t _type, uint64_t _time, uint16_t _sid, uin
 	qid(_qid) {};
 
 void Remote_message::Display(){
-	cout << "type :" << type << endl << "time :" << time << endl << "sid :" << sid << endl << "qid :" << qid << endl << endl;
+	cout << "type : " << type << endl << "time : " << time << endl << "sid : " << sid << endl << "qid : " << qid << endl << endl;
 }
 
 // PROTOCOL
