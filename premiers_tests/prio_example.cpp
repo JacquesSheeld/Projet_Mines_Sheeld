c4aca45e27eca87dd9ce295e3cb8c5ef868ac2e6
Jeremy Costanzo <jeremy.costanzo@mines-paristech.fr>
2020-04-27 17:40:32 +0200

    Custom class and comparison function for priority queue

--- premiers_tests/prio_example.cpp
+++ premiers_tests/prio_example.cpp
@@ -7,19 +7,30 @@ using namespace std;
 
 template<typename T> void print_queue(T& q) {
     while(!q.empty()) {
-        std::cout << q.top() << " ";
+        std::cout << q.top() << ", ";
         q.pop();
     }
     std::cout << '\n';
 }
- 
+
+struct mystruct{
+  int a;
+  int b;
+  mystruct(int a, int b): a(a), b(b){};
+};
+ostream& operator<< (ostream& os, const mystruct& x) {
+    os << x.a << ' ' << x.b;
+    return os;
+  }
+  
 int main(){
-  function<bool(int,int)> great = [](const int& a,const int& b){return a>b;};
-  priority_queue<int, vector<int>, decltype(great)> pq(great);
+  mystruct a (1,2);
+  mystruct b (2,1);
+  function<bool(mystruct, mystruct)> great = [](const mystruct& a,const mystruct& b){return a.b>b.b;};
+  priority_queue<mystruct, vector<mystruct>, decltype(great)> pq(great);
   cout << pq.empty() << endl;
-  pq.push(3);
-  pq.push(8);
-  pq.push(4);
+  pq.push(a);
+  pq.push(b);
   print_queue(pq);
   cout << endl;
   return 0;
